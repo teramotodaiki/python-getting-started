a095bc84e3f4f4cd10fc6a8ef3cfb04193e05461
teramotodaiki <i101323@gmail.com>
2022-06-30 14:31:00 +0900

    hiraganizeして表示

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .hiraganize import hiraganize
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
+    print("index")
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    query = request.GET.get("q", "飛沫")
+    yomi = hiraganize(query)
+    print(query, yomi)
+
+    return render(request, "index.html", {"query": query, "yomi": yomi})
 
 
 def db(request):
